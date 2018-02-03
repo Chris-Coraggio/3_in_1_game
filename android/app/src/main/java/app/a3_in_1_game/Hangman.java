03c7afd8030c074fccd4d197341be5dc6d674ca6
Muhammad Hamza Farrukh <mfarrukh@purdue.edu>
2018-02-02 19:50:00 -0500

    Added skeleton code

---
+++ android/app/src/main/java/app/a3_in_1_game/Hangman.java
@@ -0,0 +1,15 @@
+package app.a3_in_1_game;
+
+/**
+ * Created by hamza on 2/2/2018.
+ */
+
+// TODO: Code this class in IntelliJ and copy the code over
+// TODO: Use STDIN and STDOUT until we meet and implement the GUI
+// TODO: Make sure code is commented and readable
+// TODO: Make helper functions
+// TODO: Use character array to store the word
+
+public class Hangman {
+
+}
