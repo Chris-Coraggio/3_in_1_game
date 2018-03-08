c696b710d72af786616226f55cf3df94720debee
Muhammad Hamza Farrukh <mfarrukh@purdue.edu>
2018-03-08 18:25:25 -0500

    Added server stuff

--- android/app/src/main/java/app/a3_in_1_game/MainActivity.java
+++ android/app/src/main/java/app/a3_in_1_game/MainActivity.java
@@ -1,10 +1,8 @@
 package app.a3_in_1_game;
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -19,26 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button buttonTicTacToe = (Button) findViewById(R.id.buttonTicTacToe);
-        SharedPreferences sharedPref= getSharedPreferences("myPref", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor;
-        EditText nameText = (EditText) findViewById(R.id.nickname);
-        String userName =sharedPref.getString("user_id","");
-        nameText.setText(userName);
+        final EditText nameText = (EditText) findViewById(R.id.nickname);
+        final SharedPreferences sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE);
+        String user = sharedPref.getString("user", "");
+        nameText.setText(user);
 
         buttonTicTacToe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                String user = nameText.getText().toString();
+                editor.putString("user", user);
+                editor.commit();
+
                 Intent intent = new Intent(view.getContext(), Tic_Tac_Toe_Activity.class);
                 startActivity(intent);
-
-                SharedPreferences sharedPref= getSharedPreferences("myPref", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor;
-                EditText nameText = (EditText) findViewById(R.id.nickname);
-                String name = nameText.getText().toString();
-                editor = sharedPref.edit();
-                editor.putString("user_id", name);
-                editor.commit();
-                nameText.setText(name);
             }
         });
 
@@ -46,37 +39,28 @@ public class MainActivity extends AppCompatActivity {
         connect_4_launch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                String user = nameText.getText().toString();
+                editor.putString("user", user);
+                editor.commit();
+
                 Intent intent = new Intent(view.getContext(), Connect_4_Acitivity.class);
                 startActivity(intent);
-
-                SharedPreferences sharedPref= getSharedPreferences("myPref", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor;
-                EditText nameText = (EditText) findViewById(R.id.nickname);
-                String name = nameText.getText().toString();
-                editor = sharedPref.edit();
-                editor.putString("user_id", name);
-                editor.commit();
-                nameText.setText(name);
             }
         });
         Button hangman_launch = (Button) findViewById(R.id.hangman_launch);
         hangman_launch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                String user = nameText.getText().toString();
+                editor.putString("user", user);
+                editor.commit();
+
                 Intent intent = new Intent(view.getContext(), Hangman_Activity.class);
                 startActivity(intent);
-
-                SharedPreferences sharedPref= getSharedPreferences("myPref", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor;
-                EditText nameText = (EditText) findViewById(R.id.nickname);
-                 String name = nameText.getText().toString();
-                    editor = sharedPref.edit();
-                    editor.putString("user_id", name);
-                    editor.commit();
-                    nameText.setText(name);
             }
         });
-
 
 
     }
--- server/index.js
+++ server/index.js
@@ -5,29 +5,80 @@ var port = process.env.PORT || 8080;
 
 //var pool  = mysql.createPool(process.env.JAWSDB_URL);
 
+var connect_4_count = 1;
+var connect_4_games = {};
+
+function checkPlayer(user, game) {
+	if (game.host == user || game.client == user) {
+		return true;
+	}
+	return false;
+}
+
 app.get("/", function (req, res) {
     res.send("Hello World!");
 });
 
-app.route("/connect_4/:gameID")
+app.get("/connect_4_host/:host", function (req, res) {
+	var host = req.params.host
+	console.log("New Connect 4 ID: " + connect_4_count);
+	res.send(connect_4_count.toString());
+	game = {
+		gameID: connect_4_count, 
+		host: host,
+		state: "START"
+		};
+	connect_4_games[connect_4_count] = game
+	connect_4_count++;
+});
+
+app.get("/connect_4_join/:gameID/:client", function (req, res) {
+	var gameID = req.params.gameID;
+	var client = req.params.client;
+	console.log("Connect 4 ID: " + gameID);
+	console.log("Being joined by " + client);
+	if (gameID in connect_4_games) {	
+		var game = connect_4_games[gameID];
+		res.send("Game joined!");
+		game.client = client;
+		console.log(game);
+	}
+	else {
+		console.log("Error: gameID not found");
+		res.send("Error: gameID not found")
+	}
+});
+
+app.route("/connect_4/:gameID(\\d+)/:user")
 	.get(function (req, res) {
-		res.sent(req)
+		var gameID = req.params.gameID;
+		var user = req.params.user;
+		if (gameID in connect_4_games) {
+			var game = connect_4_games[gameID];
+			if (checkPlayer(user, game)) {
+				console.log(connect_4_games[req.params.gameID]);
+				res.json(connect_4_games[req.params.gameID]);
+			}
+			else {
+				console.log("Player not found in game: " + gameID);
+				res.send("Player not found in game: " + gameID);
+			}
+		}
+		else {
+			console.log("Error: gameID not found");
+			res.send("Error: gameID not found")
+		}		
 	})
 	.post(function (req, res) {
-		pool.getConnection(function(error, con) {
-			if (error) throw error;
-			var query = "INSERT INTO qdcise2a2swf0oxi.simpleapp (Operation) VALUES ('Increment');";
-			con.query(query, function (err, result) {
-				con.release();
-				if (!err) {
-					console.log("Incremented");
-					res.send("Incremented");
-				}
-				else {
-					throw err;
-				}
-			});
-		});
+		var gameID = req.params.gameID;
+		if (gameID in connect_4_games) {
+			console.log(connect_4_games[req.params.gameID]);
+			res.json(connect_4_games[req.params.gameID]);
+		}
+		else {
+			console.log("Error: gameID not found");
+			res.send("Error: gameID not found")
+		}	
 	})
 	
 app.get("/retrieve", function (req, res) {
